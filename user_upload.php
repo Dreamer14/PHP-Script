1a6aef3a3e02ec1d84448001e46d92eef1e8e2d6
dreamer14 <talwindersinghpurba@gmail.com>
2020-02-23 15:41:51 +1000

    Validated csv file data

--- user_upload.php
+++ user_upload.php
@@ -1,14 +1,13 @@
 <?php
 
 		$short_options = 'u:p:h:';
-        $long_options = array('file:','create_table','dry_run','help:');
+        $long_options = array('file:','create_table','dry_run','help');
         $options = getopt($short_options, $long_options);
       
         foreach($options as $option => $value) {
             switch ($option) {
             case 'help':
-            	show_help();
-            	exit(0);
+            	$help = true;
                 break;
              case 'file':
                $filename = $value;
@@ -35,7 +34,8 @@
              }            
                           
                           
-     echo "USER INPUT SUMMARY:\n", 
+     echo "USER INPUT SUMMARY:\n",
+    "Help: ", isset($help) ? "Yes" : "No", PHP_EOL,	
 	"Using input file: ", isset($filename) ? $filename : "No", PHP_EOL,
 	"Create table: ", isset($create_table) ? "Yes" : "No", PHP_EOL,	
 	"dry_run: ", isset($dry_run) ? "Yes" : "No", PHP_EOL,
@@ -49,7 +49,7 @@
 
 	function show_help(){
 
-		echo "help.".PHP_EOL;
+		echo "How to use: \n To create table use- 'php user_upload.php -u -p -h --create_table' \n To dry_run use- 'php user_upload.php --file users.csv --dry_run' \n To insert the data into database use- 'php user_upload.php --file users.csv -u -p -h '  ".PHP_EOL;
 	}
 	function create_table_db($host, $user, $password){
 
@@ -92,31 +92,106 @@
 
 	}
 
-	function validate_file_data(){
+	function validate_file_data($filename){
 
-		echo "validate data.\n";
+		$invalid_email[] = array();
+		$invalid_emails_count = 0;
+		$validated_data[] = array();
+		$iter_count = 0;
+		$contents = file($filename);
+		foreach ($contents as $content)
+		{
+		//this will skip the first row of csv because they are column names
+		if ( $iter_count === 0) 
+		{
+			$iter_count++;
+			continue;
+		}
+		
+		$file_row = str_getcsv($content);
+		
+		$file_row = preg_replace("/\s+/", "", $file_row);
+		// lower case each field
+		$file_row[0] = strtolower($file_row[0]);
+		$file_row[1] = strtolower($file_row[1]);
+		$file_row[2] = strtolower($file_row[2]);
+		//upper case first name and last name
+		
+		$file_row[0] = ucfirst($file_row[0]);
+		$file_row[1] = ucfirst($file_row[1]);
+		//Email Validation
+		if (preg_match("/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/", $file_row[2]) === 0) {
+			$current_iter_count = $iter_count+1;
+			array_push($invalid_email, $file_row);
+			$invalid_emails_count++;
+		}
+		//adding validated data to an array
+		array_push($validated_data, $file_row);
+		$iter_count++;
+	}
+
+		//displaying data
+		echo "Validated data \n";
+		foreach($validated_data as $data){
+
+			echo implode(",", $data), PHP_EOL;
+		}
+		//Displaying invalid Emails in the file
+		
+		if($invalid_emails_count===0){
+			echo "No invalid Emails Found";
+		}
+		else{
+			echo "\n", $invalid_emails_count, " ", "invalid Emails found";
+		foreach($invalid_email as $email){
+
+			echo implode(",", $email), PHP_EOL;
+		}
+	}
+	//remove first element of array as it contains CSV column names
+	array_shift ($validated_data);
+	//returning corrected array if there are no invalid emails
+	if ($invalid_emails_count === 0)
+	 {
+			return $validated_data;
+	} else 
+	{ // returning number of invalid emails if any.
+		return $invalid_emails_count;
+	}
 	}
 
 
-	function insert_data(){
+	function insert_data()
+	{
 
 		echo "Insert data.\n";
 	}
+
+
+
 		
 	//scenerio assumptions (therecan be other scenerios but will create ambiguity)
 
-	if(isset($create_table,$user, $password, $host) && !isset($dry_run) && !isset($filename)){
+	if(isset($create_table,$user, $password, $host) && !isset($dry_run) && !isset($filename))
+	{
 
 		create_table_db($host, $user, $password);
 	}
-	elseif(isset($dry_run,$filename) && !isset($create_table)&& !isset($user) && !isset($password) && !isset ($host)){
-		validate_file_data();
+	elseif(isset($dry_run,$filename) && !isset($create_table)&& !isset($user) && !isset($password) && !isset ($host))
+	{
+		validate_file_data($filename);
 	}
 	elseif(isset($filename,$user, $password, $host) && !isset($dry_run) && !isset($create_table)){
 
 		insert_data();
 	}
-	else { 
+	elseif(isset($help))
+	{
+
+		show_help();
+	}
+	else 
+	{ 
 	die ("Unrecognized sequence of options. please use --help for script scenerios. \n");
 }
 
